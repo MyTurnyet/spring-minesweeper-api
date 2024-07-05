abcef293f20c4ddc930e0cb9211f4d5bb8a03ccb
paige.watson <paige.watson@salesforce.com>
2024-07-05 15:20:43 -0700

    initial commit

---
+++ src/test/java/com/softwareascraft/springminesweeperapi/SpringMinesweeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareascraft.springminesweeperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMinesweeperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
