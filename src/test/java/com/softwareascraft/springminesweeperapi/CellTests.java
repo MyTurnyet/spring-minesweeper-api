9aeba7ece3a2f9ffee9110e7af3d25d8bd039e24
Ellis Lempriere <ellis@pslfamily.org>
2024-07-05 15:47:17 -0700

    Add Cell test configuration file

---
+++ src/test/java/com/softwareascraft/springminesweeperapi/CellTests.java
@@ -0,0 +1,15 @@
+package com.softwareascraft.springminesweeperapi;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Tag("Unit")
+public class CellTests {
+
+    @Test
+    void testCell() {
+        assertThat(true).isTrue();
+    }
+}
