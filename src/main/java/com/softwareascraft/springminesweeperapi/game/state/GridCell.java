56a24a44946f360fa79857accbce3b0d577dc4f9
paige.watson <paige.watson@salesforce.com>
2024-08-23 08:29:15 -0700

    refactor to add factory constructor

--- src/main/java/com/softwareascraft/springminesweeperapi/game/state/GridCell.java
+++ src/main/java/com/softwareascraft/springminesweeperapi/game/state/GridCell.java
@@ -1,16 +1,37 @@
 package com.softwareascraft.springminesweeperapi.game.state;
 
+import com.softwareascraft.springminesweeperapi.cells.Cell;
+import com.softwareascraft.springminesweeperapi.cells.EmptyCell;
+import com.softwareascraft.springminesweeperapi.cells.MineCell;
+
 public class GridCell {
 
+    public static GridCell withMine(int row, int column){
+        return new GridCell(row, column, new MineCell());
+    }
+    public static GridCell empty(int row, int column){
+        return new GridCell(row, column, new EmptyCell());
+    }
     private final int rowIndex;
     private final int columnIndex;
+    private final Cell cell;
 
-    public GridCell(int row, int column) {
+    private GridCell(int row, int column) {
+        this(row, column, new EmptyCell());
+    }
+
+    private GridCell(int row, int column, Cell cell) {
         this.rowIndex = row;
         this.columnIndex = column;
+        this.cell = cell;
     }
+
 
     public boolean isAt(int row, int column) {
         return this.rowIndex == row && this.columnIndex == column;
+    }
+
+    public boolean containsMine() {
+        return this.cell.containsMine();
     }
 }
--- src/test/java/com/softwareascraft/springminesweeperapi/game/state/GridCellTests.java
+++ src/test/java/com/softwareascraft/springminesweeperapi/game/state/GridCellTests.java
@@ -1,5 +1,8 @@
 package com.softwareascraft.springminesweeperapi.game.state;
 
+import com.softwareascraft.springminesweeperapi.cells.Cell;
+import com.softwareascraft.springminesweeperapi.cells.EmptyCell;
+import com.softwareascraft.springminesweeperapi.cells.MineCell;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -10,19 +13,31 @@ public class GridCellTests {
 
     @Test
     void createsWithCoordinates() {
-        GridCell gridCell = new GridCell(0, 0);
+        GridCell gridCell = GridCell.empty(0,0);
         assertThat(gridCell.isAt(0, 0)).isTrue();
     }
-    
+
     @Test
     void isNotInGivenRow() {
-        GridCell gridCell = new GridCell(0, 0);
+        GridCell gridCell = GridCell.empty(0, 0);
         assertThat(gridCell.isAt(1, 0)).isFalse();
     }
 
     @Test
     void isNotInGivenColumn() {
-        GridCell gridCell = new GridCell(0, 0);
+        GridCell gridCell = GridCell.empty(0, 0);
         assertThat(gridCell.isAt(0, 1)).isFalse();
+    }
+
+    @Test
+    void gridcellWithEmptyCellDoesNotContainMine() {
+        GridCell gridCell = GridCell.empty(0, 0);
+        assertThat(gridCell.containsMine()).isFalse();
+    }
+
+    @Test
+    void gridcellWithMineCellContainsMine() {
+        GridCell gridCell = GridCell.withMine(0, 0);
+        assertThat(gridCell.containsMine()).isTrue();
     }
 }
