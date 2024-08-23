66748d69c1658b6fe7965c16d5f4c586fb573565
paige.watson <paige.watson@salesforce.com>
2024-08-23 08:30:05 -0700

    remove spacing issues

--- src/main/java/com/softwareascraft/springminesweeperapi/game/state/GridCell.java
+++ src/main/java/com/softwareascraft/springminesweeperapi/game/state/GridCell.java
@@ -6,26 +6,23 @@ import com.softwareascraft.springminesweeperapi.cells.MineCell;
 
 public class GridCell {
 
-    public static GridCell withMine(int row, int column){
+    public static GridCell withMine(int row, int column) {
         return new GridCell(row, column, new MineCell());
     }
-    public static GridCell empty(int row, int column){
+
+    public static GridCell empty(int row, int column) {
         return new GridCell(row, column, new EmptyCell());
     }
+
     private final int rowIndex;
     private final int columnIndex;
     private final Cell cell;
-
-    private GridCell(int row, int column) {
-        this(row, column, new EmptyCell());
-    }
 
     private GridCell(int row, int column, Cell cell) {
         this.rowIndex = row;
         this.columnIndex = column;
         this.cell = cell;
     }
-
 
     public boolean isAt(int row, int column) {
         return this.rowIndex == row && this.columnIndex == column;
