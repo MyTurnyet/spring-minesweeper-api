32a0af4906e5d4632f534c9a1b4ebcaed8bd8341
Ellis Lempriere <ellis@pslfamily.org>
2025-02-07 16:18:37 -0800

    clean up unused imports

--- src/main/java/com/softwareascraft/springminesweeperapi/game/state/Grid.java
+++ src/main/java/com/softwareascraft/springminesweeperapi/game/state/Grid.java
@@ -4,8 +4,6 @@ import com.softwareascraft.springminesweeperapi.cells.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Grid {
 
